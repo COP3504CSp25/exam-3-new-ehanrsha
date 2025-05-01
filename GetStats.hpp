0cff8438580c0d8cbaeb680d6452fae78c63eb0c
Ehan Shah <ehanrsha@gmail.com>
2025-05-01 19:29:36 +0000

    first commit

--- GetStats.hpp
+++ GetStats.hpp
@@ -14,7 +14,42 @@ public:
     }
 
     int* getStats() const{
-        //write your function here!
+        int* newArray = new int[3];
+        
+        int min = 0;
+        int max = 0;
+        int mean = 0;
+        int count = 0;
+
+        if (head == nullptr){
+            newArray[0] = min;
+            newArray[1] = max;
+            newArray[2] = mean;
+
+            return newArray;
+        }
+
+        Node* currentNode = head;
+        min = currentNode->data;
+        max = currentNode->data;
+        while(currentNode != nullptr){
+            if (currentNode->data < min){
+                min = currentNode->data;
+            }
+            if (currentNode->data > max){
+                max = currentNode->data;
+            }
+            mean += currentNode->data;
+            count++;
+        }
+
+        mean = mean / count ;
+
+        newArray[0] = min;
+        newArray[1] = max;
+        newArray[2] = mean;
+
+        return newArray;
     }
     
 
